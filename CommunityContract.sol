68682a7c0ccf2a9dab0cb5f54f6530d89f728aa4
Veronika <58723287+interestIngc@users.noreply.github.com>
2025-05-20 22:19:58 +0200

    first version done

---
+++ CommunityContract.sol
@@ -0,0 +1,286 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CommunityContract {
+    // This should be a MultiSignerERC7913Weighted multisig account
+    address public admin;
+
+    // ======= Admin Management =======
+    
+    event AdminChanged(address indexed previousAdmin, address indexed newAdmin);
+    
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only admin can call this function");
+        _;
+    }
+    
+    constructor(address _admin) {
+        require(_admin != address(0), "Admin address cannot be zero");
+        admin = _admin;
+        emit AdminChanged(address(0), _admin);
+    }
+    
+    function changeAdmin(address _newAdmin) external onlyAdmin {
+        require(_newAdmin != address(0), "New admin address cannot be zero");
+        address oldAdmin = admin;
+        admin = _newAdmin;
+        emit AdminChanged(oldAdmin, _newAdmin);
+    }
+
+    // ======= Item Management =======
+
+    struct Item {
+        // Base item settings
+        bytes32 itemHash;
+        uint256 initialPrice;
+        uint256 minimalPrice;
+        uint256 depreciationRate;
+        uint256 depreciationInterval;
+        uint256 lastReleaseTimestamp;
+        // if item gets rerented by another citizen, this is the notice period for the current item holder
+        uint256 releaseInterval;
+        uint256 taxRate;
+
+        // Info about the current rent
+        bool isRented;
+        address rentedByCitizenAddress;
+        uint256 currentPrice;
+    }
+
+    mapping(uint256 => Item) public items;
+    uint256 public itemCount;
+    
+    event ItemAdded(uint256 indexed itemId, bytes32 itemHash, uint256 initialPrice, uint256 minimalPrice, uint256 depreciationRate, uint256 depreciationInterval, uint256 taxRate);
+    event ItemRented(uint256 indexed itemId, address indexed renter, uint256 price);
+    event ItemReleased(uint256 indexed itemId, address indexed renter);
+
+    error ErrorItemDoesNotExist(uint256 itemId);
+    error ErrorInsufficientBalanceToRent(uint256 required, uint256 available);
+    error ErrorPaymentFailed();
+    error ErrorItemIsNotRented(uint256 itemId);
+    error ErrorItemIsNotRentedBySender(uint256 itemId, address sender);
+
+    function addItem(
+        bytes32 _itemHash,
+        uint256 _initialPrice,
+        uint256 _minimalPrice,
+        uint256 _depreciationRate,
+        uint256 _depreciationInterval,
+        uint256 _releaseInterval,
+        uint256 _taxRate
+    ) external onlyAdmin {
+        require(_initialPrice > 0, "Initial price must be greater than 0");
+        require(_minimalPrice > 0, "Minimal price must be greater than 0");
+        require(_minimalPrice <= _initialPrice, "Minimal price cannot be greater than initial price");
+        require(_depreciationRate > 0, "Depreciation rate must be greater than 0");
+        require(_depreciationInterval > 0, "Depreciation interval must be greater than 0");
+        
+        uint256 itemId = itemCount;
+        items[itemId] = Item({
+            itemHash: _itemHash,
+            initialPrice: _initialPrice,
+            minimalPrice: _minimalPrice,
+            depreciationRate: _depreciationRate,
+            depreciationInterval: _depreciationInterval,
+            taxRate: _taxRate,
+            // Initially lastReleaseTimestamp is equal to creation timestamp
+            lastReleaseTimestamp: block.timestamp,
+            releaseInterval: _releaseInterval,
+            isRented: false,
+            rentedByCitizenAddress: address(0),
+            currentPrice: _initialPrice
+        });
+        
+        itemCount++;
+        
+        emit ItemAdded(itemId, _itemHash, _initialPrice, _minimalPrice, _depreciationRate, _depreciationInterval, _taxRate);
+    }
+
+    function getItem(uint256 _itemId) external view returns (Item memory) {
+        Item storage item = items[_itemId];
+        if (item.lastReleaseTimestamp == 0) {
+            revert ErrorItemDoesNotExist(_itemId);
+        }
+
+        return item;
+    }
+
+    function rentItem(uint256 _itemId, uint256 _newPrice) external payable {
+        Item storage item = items[_itemId];
+
+        if (item.lastReleaseTimestamp == 0) {
+            revert ErrorItemDoesNotExist(_itemId);
+        }
+
+        uint256 currentPrice = getItemPrice(_itemId);
+
+        if (msg.value != currentPrice) {
+            revert ErrorInsufficientBalanceToRent(currentPrice, msg.value);
+        }
+
+        uint256 citizenId = citizenIdByAddress[msg.sender];
+
+        (bool success, ) = admin.call{value: msg.value}("");
+        if (!success) {
+            revert ErrorPaymentFailed();
+        }
+
+        item.currentPrice = _newPrice;
+        item.rentedByCitizenAddress = msg.sender;
+        item.isRented = true;
+
+        citizens[citizenId].rentedItems.push(RentedItemMetadata({
+            itemId: _itemId,
+            rentedFrom: block.timestamp,
+            rentedUntil: 0,
+            price: msg.value,
+            lastTaxUpdateTimestamp: block.timestamp
+        }));
+
+        emit ItemRented(_itemId, msg.sender, msg.value);
+    }
+
+    function releaseItem(uint256 _itemId) external {
+        Item storage item = items[_itemId];
+
+        if (item.lastReleaseTimestamp == 0) {
+            revert ErrorItemDoesNotExist(_itemId);
+        }
+
+        if (!item.isRented) {
+            revert ErrorItemIsNotRented(_itemId);
+        }
+
+        if (item.rentedByCitizenAddress != msg.sender) {
+            revert ErrorItemIsNotRentedBySender(_itemId, msg.sender);
+        }
+
+        (bool success, ) = item.rentedByCitizenAddress.call{value: item.currentPrice}("");
+        if (!success) {
+            revert ErrorPaymentFailed();
+        }
+
+        item.isRented = false;
+        item.lastReleaseTimestamp = block.timestamp;
+        item.rentedByCitizenAddress = address(0);
+
+        uint256 citizenId = citizenIdByAddress[msg.sender];
+        for (uint256 i = 0; i < citizens[citizenId].rentedItems.length; i++) {
+            RentedItemMetadata storage rentedItem = citizens[citizenId].rentedItems[i];
+            if (rentedItem.itemId == _itemId) {
+                rentedItem.rentedUntil = block.timestamp;
+                break;
+            }
+        }
+
+        emit ItemReleased(_itemId, msg.sender);
+    }
+
+    function getCurrentItemRenter(uint256 _itemId) public view returns (address) {
+        Item storage item = items[_itemId];
+        if (item.lastReleaseTimestamp == 0) {
+            revert ErrorItemDoesNotExist(_itemId);
+        }
+
+        if (!item.isRented) {
+            return address(0);
+        }
+        
+        return item.rentedByCitizenAddress;
+    }
+
+    function getItemPrice(uint256 _itemId) public view returns (uint256) {
+        Item storage item = items[_itemId];
+        if (item.lastReleaseTimestamp == 0) {
+            revert ErrorItemDoesNotExist(_itemId);
+        }
+
+        if (item.isRented) {
+            return item.currentPrice;
+        }
+
+        // item is unoccupied, apply depreciation logic based on the Dutch Auction model
+
+        uint256 depreciation = (block.timestamp - item.lastReleaseTimestamp) / item.depreciationInterval;
+        uint256 depreciatedPrice = item.initialPrice - item.depreciationRate * depreciation;
+
+        if (depreciatedPrice < item.minimalPrice) {
+            return item.minimalPrice;
+        }
+
+        return depreciatedPrice;
+    }
+    
+    // ======= Citizen Management =======
+    
+    struct Citizen {
+        address citizenAddress;
+        uint256 balance;
+        uint256 tokens;
+        RentedItemMetadata[] rentedItems;
+    }
+
+    struct RentedItemMetadata {
+        uint256 itemId;
+        uint256 rentedFrom;
+        uint256 rentedUntil;
+        uint256 price;
+
+        uint256 lastTaxUpdateTimestamp;
+    }
+    
+    mapping(uint256 => Citizen) public citizens;
+    uint256 public citizenCount;
+
+    mapping(address => uint256) public citizenIdByAddress;
+    
+    event CitizenRegistered(address indexed citizenAddress, uint256 indexed citizenId);
+    
+    function registerCitizen(address _citizenAddress, uint256 initialBalance) external {                
+        citizens[citizenCount] = Citizen({
+            citizenAddress: _citizenAddress,
+            balance: initialBalance,
+            tokens: 0,
+            rentedItems: new RentedItemMetadata[](0)
+        });
+        citizenIdByAddress[_citizenAddress] = citizenCount;
+
+        citizenCount++;
+        
+        emit CitizenRegistered(msg.sender, citizenCount);
+    }
+
+    /* This function is triggered periodically to pay tax for each citizen
+    */
+    function payTaxes() external {
+        for (uint256 i = 0; i < citizenCount; i++) {
+            Citizen storage citizen = citizens[i];
+            for (uint256 j = 0; j < citizen.rentedItems.length; j++) {
+                RentedItemMetadata storage rentedItem = citizen.rentedItems[j];
+
+                uint256 usage = block.timestamp - rentedItem.lastTaxUpdateTimestamp;
+                if (rentedItem.rentedUntil != 0) {
+                    usage = rentedItem.rentedUntil - rentedItem.lastTaxUpdateTimestamp;
+                }
+
+                uint256 amountToPay = rentedItem.price * (items[rentedItem.itemId].taxRate / 100) * usage / (block.timestamp - rentedItem.lastTaxUpdateTimestamp);
+
+                citizen.balance -= amountToPay;
+                citizen.tokens += amountToPay;
+
+                rentedItem.lastTaxUpdateTimestamp = block.timestamp;
+            }
+
+            // delete released items from the citizen's rentedItems array
+            for (uint256 j = 0; j < citizen.rentedItems.length; j++) {
+                RentedItemMetadata storage rentedItem = citizen.rentedItems[j];
+                if (rentedItem.rentedUntil != 0) {
+                    for (uint256 k = j + 1; k < citizen.rentedItems.length; k++) {
+                        citizen.rentedItems[k - 1] = citizen.rentedItems[k];
+                    }
+                    citizen.rentedItems.pop();
+                }
+            }
+        }
+    }
+}
